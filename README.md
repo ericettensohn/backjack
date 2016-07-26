884cd435865379d06eb62c721f78932406ab8463
ericettensohn <ericettensohn@gmail.com>
2016-07-26 16:39:40 -0400

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+#Blackjack game using jQuery, HTML and CSS.
+
+##The JS has three primary buttons: deal, hit and stand.
+
+###The table is set up with Bootstrap.
---
+++ js/scripts.js
@@ -0,0 +1,216 @@
+// set nessages after game over
+// design
+// fix face cards
+// fix aces
+// player can hit forever
+// no win counter
+// cards arent red or black
+// cards dont look good
+// no delay on showing cards
+// dealers 2nd card is visible on deal
+
+
+
+// when the user clicks deal - deal
+
+	var theDeck = [];
+	// var playerTotal = 0;
+	// var dealerTotal = 0;
+	var playersHand = [];
+	var dealersHand = [];
+	var topOfTheDeck = 4;
+	var dealerTotal = 0;
+	var playerTotal = 0;
+
+$(document).ready(function() {
+
+	
+
+
+
+	$(".deal-button").click(function(){
+		createDeck(); // create array of 1H-13c
+		shuffleDeck();
+
+
+		playersHand.push(theDeck[0]);
+		placeCard('player', 'one', theDeck[0]);
+		
+		dealersHand.push(theDeck[1]);
+		placeCard('dealer', 'one', theDeck[1]);
+		
+		playersHand.push(theDeck[2]);
+		placeCard('player', 'two', theDeck[2]);
+		
+		dealersHand.push(theDeck[3]);
+		placeCard('dealer', 'two', theDeck[3]);
+
+		calculateTotal(playersHand, "player");
+		calculateTotal(dealersHand, "dealer");
+
+	}); // deal button listener
+
+	$(".hit-button").click(function(){
+		// placeCard('player', 'three', theDeck[4])
+		var slot = '';
+		if(playersHand.length == 2) {
+			slot = "three";
+		}
+		else if (playersHand.length == 3) {
+			slot = "four";
+		}
+		else if (playersHand.length == 4) {
+			slot = "five";
+		}
+		else if (playersHand.length == 5) {
+			slot = "six";
+		}
+
+		if (playersHand.length < 6) {
+			placeCard('player', slot, theDeck[topOfTheDeck]);
+			playersHand.push(theDeck[topOfTheDeck]);
+			calculateTotal(playersHand, 'player');
+			topOfTheDeck ++;
+		}
+		if (playerTotal >= 21) {
+			
+
+			checkWin()
+		}
+
+		
+
+
+	}); // hit button listener
+
+	$(".stand-button").click(function(){
+		var slot = '';
+		dealerTotal = calculateTotal(dealersHand, 'dealer');
+		while(dealerTotal < 17) {
+			if(dealersHand.length == 2) {
+				slot = "three";
+			}
+			else if(dealersHand.length == 3) {
+				slot = "four";
+			} 
+			else if(dealersHand.length == 4) {
+				slot = "five";
+			}
+			else if(dealersHand.length == 5) {
+				slot = "six";
+			}
+			placeCard('dealer', slot, theDeck[topOfTheDeck]);
+			dealersHand.push(theDeck[topOfTheDeck]);
+			dealerTotal = calculateTotal(dealersHand, 'dealer');
+			topOfTheDeck++;
+		}
+
+		// dealer has at least 17, check to see who won
+		checkWin();
+
+	}); // stand button listener
+});
+
+function checkWin() {
+	playerTotal = calculateTotal(playersHand, 'player');
+	dealerTotal = calculateTotal(dealersHand, 'dealer');
+	console.log(playerTotal)
+
+	var result;
+
+	if(playerTotal > 21) {
+		// player has busted
+
+		result = "player bust"
+
+	}
+	else if (dealerTotal > 21) {
+		// dealer has busted
+		result = "dealer bust"
+	}
+	else {
+		// neither player has more than 21
+		if(playerTotal > dealerTotal) {
+			// player won
+			result = "player won"
+		}
+		else if (dealerTotal > playerTotal) {
+			// dealer won
+			result = "dealer won"
+		}
+		else {
+			// tie
+			result = "tie"
+		}
+	}
+	$('.game-result').html(result)
+	return result;
+
+}
+
+function placeCard(who, where, cardToPlace) {
+	var classSelector = '.' + who + '-cards .card-' + where;
+	
+
+	// fix face card issue
+
+	$(classSelector).html(cardToPlace);
+
+
+}
+
+function createDeck() {
+	// Fill deck with 52 cards, 4 suits (h, s, d, c)
+	var suits = ['h', 's', 'd', 'c']
+	for (var s = 0; s < suits.length; s++) {
+		for (var c = 1; c <= 13; c++) {
+			theDeck.push(c + suits[s]);
+		}
+	}
+}
+
+function shuffleDeck() {
+	for (var i = 1; i < 1000; i++) {
+		card1 = Math.floor(Math.random() * theDeck.length);
+		card2 = Math.floor(Math.random() * theDeck.length);
+		var temp = theDeck[card1];
+		theDeck[card1] = theDeck[card2];
+		theDeck[card2] = temp;
+	}
+}
+
+function calculateTotal(hand, whosTurn) {
+	var cardValue = 0;
+	var total = 0;
+	for(var i = 0; i < hand.length; i++) {
+		cardValue = Number(hand[i].slice(0, -1))
+		if(cardValue == 1 && aceSearch() == false) {
+			cardValue = 11
+		} else if (cardValue > 10) {
+			cardValue = 10;
+		} else if (cardValue == 1 && aceSearch() == true) {
+			cardValue = 1;
+		}
+
+		total += cardValue;
+	}
+
+	if (whosTurn == 'player') {
+		playerTotal = total;
+	}
+
+	// set player OR dealer total number
+	var elementToUpdate = '.' + whosTurn + '-total-number';
+	$(elementToUpdate).html(total);
+	return total;
+}
+
+function aceSearch(array) {
+	for (var i = 0; i < playersHand.length; i++) {
+		if(playerTotal > 21 && (parseInt(playersHand[i]) == 1)) {
+			return true
+		} else {
+			return false
+		}
+	}
+}
